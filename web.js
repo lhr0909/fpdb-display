4ba7065ca66d58760a9f0e601d145224638f091f
Simon Liang <no.lhr0909@gmail.com>
2014-04-03 02:38:34 -0400

    Added Tourney Graph API

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var mysql  = require('mysql');
 var express = require('express');
 var cors = require('cors');
 
-var getGraphData = function(callback) {
+var runQuery = function(query, callback) {
   var connection = mysql.createConnection({
     host     : 'localhost',
     user     : constants.username,
@@ -14,21 +14,34 @@ var getGraphData = function(callback) {
   
   connection.connect();
   
-  connection.query("SELECT hp.handId, hp.totalProfit, hp.sawShowdown, hp.allInEV FROM HandsPlayers hp INNER JOIN Players pl ON  (pl.id = hp.playerId) INNER JOIN Hands h ON  (h.id  = hp.handId) INNER JOIN Gametypes gt ON  (gt.id = h.gametypeId) WHERE pl.id in (1) AND   pl.siteId in (21) AND   h.startTime > '1970-01-02 00:00:00' AND   h.startTime < '2100-12-12 23:59:59' AND (gt.limitType = 'nl' and gt.bigBlind in (5)) and gt.category in ('holdem') AND gt.currency in ('USD') GROUP BY h.startTime, hp.handId, hp.sawShowdown, hp.totalProfit, hp.allInEV ORDER BY h.startTime", function(err, rows, fields) {
+  connection.query(query, function(err, rows, fields) {
     if (err) throw err;
-  
     callback(rows, fields);
   });
   
   connection.end();
-}
+};
+
+var getCashGraphData = function(callback) {
+  runQuery("SELECT hp.handId, hp.totalProfit, hp.sawShowdown, hp.allInEV FROM HandsPlayers hp INNER JOIN Players pl ON (pl.id = hp.playerId) INNER JOIN Hands h ON (h.id  = hp.handId) INNER JOIN Gametypes gt ON (gt.id = h.gametypeId) WHERE pl.id in (1) AND   pl.siteId in (21) AND   h.startTime > '1970-01-02 00:00:00' AND   h.startTime < '2100-12-12 23:59:59' AND (gt.limitType = 'nl' and gt.bigBlind in (5)) and gt.category in ('holdem') AND gt.currency in ('USD') GROUP BY h.startTime, hp.handId, hp.sawShowdown, hp.totalProfit, hp.allInEV ORDER BY h.startTime", callback);
+};
+
+var getTourneyGraphData = function(callback) {
+  runQuery("SELECT tp.tourneyId, (coalesce(tp.winnings,0) - coalesce(tt.buyIn,0) - coalesce(tt.fee,0)) as profit, tp.koCount, tp.rebuyCount, tp.addOnCount, tt.buyIn, tt.fee, t.siteTourneyNo FROM TourneysPlayers tp INNER JOIN Players pl ON (pl.id = tp.playerId) INNER JOIN Tourneys t ON (t.id  = tp.tourneyId) INNER JOIN TourneyTypes tt ON (tt.id = t.tourneyTypeId) WHERE pl.id in (1) AND   pl.siteId in (21) AND   (t.startTime > '1970-01-02 00:00:00' AND t.startTime < '2100-12-12 23:59:59') AND   tt.currency = 'USD' GROUP BY t.startTime, tp.tourneyId, tp.winningsCurrency, tp.winnings, tp.koCount, tp.rebuyCount, tp.addOnCount, tt.buyIn, tt.fee, t.siteTourneyNo ORDER BY t.startTime", callback);
+};
 
 var app = express();
 
 app.use(cors());
 
 app.get('/fpdb-display/cash_graph.json', function(req, res) {
-  getGraphData(function(rows, fields) {
+  getCashGraphData(function(rows, fields) {
+    res.send(rows);
+  });
+});
+
+app.get('/fpdb-display/tourney_graph.json', function(req, res) {
+  getTourneyGraphData(function(rows, fields) {
     res.send(rows);
   });
 });
